d927a738843f7d5bc0a43b80460fe2cc8d63f0ca
skye <skyesajous@gmail.com>
2020-03-05 11:17:51 -0700

    trying out my API

--- water-my-plants/src/utils/axiosWithAuth.js
+++ water-my-plants/src/utils/axiosWithAuth.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const getToken = () => {
-    localStorage.getItem('token');
+  localStorage.getItem("token");
 };
 
 const axiosWithAuth = () => {
-    return axios.create({
-        //config object, will need to update this to our baseURL herokuapp from backend
-        baseURL: 'https://wmp3.herokuapp.com/api',
-        headers: {
-            Authorization: localStorage.getItem('token')
-        }
-    });
+  return axios.create({
+    //config object, will need to update this to our baseURL herokuapp from backend
+    baseURL: "https://wmp3.herokuapp.com/api",
+    headers: {
+      Authorization: localStorage.getItem("token")
+    }
+  });
 };
 
 export default axiosWithAuth;
